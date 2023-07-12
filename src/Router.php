7a6881f29aafdd01a8df64af50879415967f2769
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-07-12 22:37:40 +0100

    Create Router.php

---
+++ src/Router.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types = 1);
+
+namespace Logadapp\Router;
+
+/**
+ * Class Router
+ * @author Michael Arawole <michael@logad.net>
+ * @package logadapp
+ */
+final class Router
+{
