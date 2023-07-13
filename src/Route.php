af6722ef2aba17e12a1d0b12274f723112f00620
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-07-13 20:14:41 +0100

    Added `path` property, get & set methods

--- src/Route.php
+++ src/Route.php
@@ -4,4 +4,16 @@ namespace LogadApp\Router;
 
 final class Route
 {
+    private string $path;
+    public function setPath(string $path):self
+    {
+        $this->path = $path;
+        return $this;
+    }
+
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
 }
